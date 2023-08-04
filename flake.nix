204c70ab2170a6d9f6fe88c9b30321f7c2b96d8c
Nathan Regner <nathanregner@gmail.com>
2023-08-04 09:10:03 -0600

    First pass at Orange Pi 5 kernel package

--- flake.nix
+++ flake.nix
@@ -2,18 +2,25 @@
   description = "Orange Pi Linux Kernels";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
 
-    orange-pi-5-10-rk3588 = {
-      url = "github:orangepi-xunlong/linux-orangepi?ref=orange-pi-5.10-rk3588";
+    linux-orange-pi-6-5-rk3588 = {
+      url = "git://git.kernel.org/pub/scm/linux/kernel/git/sre/linux-misc.git";
+      flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: {
-    # TODO: aarch64 support
-    packages = flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in rec { default = self.packages.x86_64-linux.hello; });
-  };
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    # cross-compile on more powerful host system
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (hostSystem:
+      let
+        hostPkgs = nixpkgs.legacyPackages.${hostSystem};
+        targetPkgs = hostPkgs.pkgsCross.aarch64-multiplatform;
+        inherit (targetPkgs) callPackage;
+      in {
+        packages = {
+          linux-orange-pi-6-5-rk3588 =
+            callPackage ./linux/orange-pi-6.5-rk3588 { inherit inputs; };
+        };
+      });
 }
