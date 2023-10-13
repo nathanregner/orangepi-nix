fbdca1dae3d1dd4fb2ea11648101d5e52daaa497
Nathan Regner <nathanregner@gmail.com>
2023-10-12 20:57:53 -0600

    WIP ccache

--- linux/rockchip-rk3588-edge/default.nix
+++ linux/rockchip-rk3588-edge/default.nix
@@ -1,12 +1,17 @@
 { inputs, lib, pkgsBuildBuild, linuxManualConfig, linuxPackages_latest
+, pkgsHostTarget, pkgsBuildTarget, buildPackages # FIXME: pkgsBuildTarget
 , writeTextFile, ... }@args:
 let
+  inherit (pkgsBuildBuild) clangStdenv ccacheStdenv;
   inherit (pkgsBuildBuild.llvmPackages_16) bintools-unwrapped clang;
   inherit (linuxPackages_latest) kernel;
+
+  stdenv = ccacheStdenv.override { stdenv = clangStdenv; };
+
   # https://github.com/armbian/build/blob/103d8403078c149334a8454adda1641f1151f4c5/config/sources/families/rockchip-rk3588.conf#L32
   patchesPath = "${inputs.armbian-build}/patch/kernel/rockchip-rk3588-edge/";
 in with lib;
-(linuxManualConfig rec {
+((linuxManualConfig rec {
   # TODO: pin to version that Armbian build is expecting...
   inherit (kernel) version modDirVersion src;
   extraMeta = {
@@ -40,7 +45,11 @@ in with lib;
 
   # build with Clang for easier cross-compilation
   extraMakeFlags = [ "LLVM=1" "CROSS_COMPILE=aarch64-linux-gnu-" ];
-} // (args.argsOverride or { })).overrideAttrs (final: prev: {
+} // (args.argsOverride or { })).override {
+  # stdenv = (pkgsBuildTarget.ccacheStdenv.override {
+  #   stdenv = pkgsBuildTarget.clangStdenv;
+  # });
+}).overrideAttrs (final: prev: {
 
   preConfigure = ''
     ${prev.preConfigure or ""}
@@ -68,7 +77,21 @@ in with lib;
     }" >> ${dtsPath}/Makefile;
   '';
 
-  nativeBuildInputs = prev.nativeBuildInputs ++ [ bintools-unwrapped clang ];
+  depsBuildBuild = let
+    inherit (pkgsBuildBuild.llvmPackages_16) stdenv bintools-unwrapped clang;
+  in [
+    (buildPackages.ccacheStdenv.override { inherit stdenv; }).cc
+    bintools-unwrapped
+    clang
+  ];
+
+  nativeBuildInputs = let
+    inherit (pkgsBuildBuild.llvmPackages_16) stdenv bintools-unwrapped clang;
+  in [
+    (pkgsBuildBuild.ccacheStdenv.override { inherit stdenv; }).cc
+    bintools-unwrapped
+    clang
+  ] ++ prev.nativeBuildInputs;
 
   # remove CC=stdenv.cc flag
   makeFlags = filter (flag: !(strings.hasPrefix "CC=" flag)) prev.makeFlags;
